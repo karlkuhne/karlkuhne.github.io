534ea74311ce2f83cdc73d807b597b54183d298f
Karl Kuhne <karlkuhne@t-online.de>
2024-08-30 16:31:49 +0100

    hmm

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -1,3 +1,5 @@
+//PARALLAX
+
 document.addEventListener('scroll', function () {
   var scrolled = window.scrollY;
   var parallax = document.querySelector('.parallax');
@@ -5,58 +7,8 @@ document.addEventListener('scroll', function () {
   parallax.style.backgroundPositionY = offset + 'px';
 });
 
-/* document.addEventListener("DOMContentLoaded", () => {
-    const headers = document.querySelectorAll("#galery-header, #about-header, #contact-header");
 
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            const rect = entry.boundingClientRect;
-            const element = entry.target;
-
-            if (rect.top < window.innerHeight && rect.bottom > 0) {
-                // Element ist im Viewport
-                element.classList.add("fade-in-text");
-            } else {
-                // Entferne die Klasse `fade-in-text`, wenn das Element den Viewport verlassen hat und vollständig unterhalb des Viewports ist
-                if (rect.top >= window.innerHeight && rect.bottom > window.innerHeight) {
-                    element.classList.remove("fade-in-text");
-                }
-            }
-        });
-    });
-
-    headers.forEach(header => observer.observe(header));
-}); */
-
-
-
-
-/* document.addEventListener("DOMContentLoaded", function () {
-    const fadeInOnScroll = (elementSelector, offsetRem = 0) => {
-      const elements = document.querySelectorAll(elementSelector);
-  
-      elements.forEach(element => {
-        const rect = element.getBoundingClientRect();
-        const offsetPx = offsetRem * rootFontSize; // Umrechnung von rem in px
-        if (rect.top < window.innerHeight - offsetPx && rect.bottom > offsetPx) {
-          element.classList.add('fade-in-text');
-        } else {
-          // Entferne die Klasse `fade-in`, wenn das Element den Viewport verlassen hat und vollständig unterhalb des Viewports ist
-          if (rect.top >= window.innerHeight - offsetPx && rect.bottom > window.innerHeight) {
-            element.classList.remove('fade-in-text');
-          }
-        }
-  
-      });
-    };
-  
-    fadeInOnScroll('#about-header', 30);
-  
-    window.addEventListener('scroll', () => {
-      fadeInOnScroll('#about-header', 30);
-      
-    });
-  }); */
+// BURHFER MENU
 
 const hamburger = document.querySelector("#hamburger");
 const mainNav = document.querySelector("#main-nav");
@@ -73,3 +25,21 @@ mainNavItem.forEach((link) => {
     mainNav.classList.remove("active");
   })
 })
+
+
+// MAIN WIDTH
+
+function getCSSVariable(variableName) {
+  return getComputedStyle(document.documentElement).getPropertyValue(variableName).trim();
+}
+
+function setInitialMainWidth() {
+  const screenWidth = window.screen.width;
+
+  const InitialMainWidthPercentage = parseInt(getCSSVariable('--main-width'));
+  const InitialMainWidth = (screenWidth * InitialMainWidthPercentage) / 100;
+  document.documentElement.style.setProperty('--main-width', `${InitialMainWidth}px`);
+}
+
+setInitialMainWidth();
+window.addEventListener('body.load', setInitialMainWidth);
