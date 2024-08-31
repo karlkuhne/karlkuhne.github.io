d52df487641bfcf9b5249cb8ecbce48eedd43edc
Karl Kuhne <karlkuhne@t-online.de>
2024-08-31 02:15:20 +0100

    das hat doch ne 1 verdient

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -29,19 +29,42 @@ mainNavItem.forEach((link) => {
 
 // MAIN WIDTH
 
+let InitialMainWidth;
+
 function getCSSVariable(variableName) {
   return getComputedStyle(document.documentElement).getPropertyValue(variableName).trim();
 }
 
-function setInitialMainWidth() {
-  const screenWidth = window.screen.width;
-
-  const InitialMainWidthPercentage = parseInt(getCSSVariable('--main-width'));
-  const InitialMainWidth = (screenWidth * InitialMainWidthPercentage) / 100;
-  document.documentElement.style.setProperty('--main-width', `${InitialMainWidth}px`);
-
-  console.log(`Initial main width set to ${InitialMainWidth}px`);
+function getRemInPixels() {
+  return parseFloat(getComputedStyle(document.documentElement).fontSize);
 }
 
-setInitialMainWidth();
-window.addEventListener('body.load', setInitialMainWidth);
+function setInitialMainWidth() {
+  const screenWidth = window.screen.width;
+  const InitialMainWidthPercentage = parseInt(getCSSVariable('--main-width'), 10);
+  InitialMainWidth = (screenWidth * InitialMainWidthPercentage) / 100;
+  document.documentElement.style.setProperty('--main-width', `${InitialMainWidth}px`);
+
+  console.log(InitialMainWidth);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  setInitialMainWidth();
+  adjustLayout();
+});
+
+// RESPONSIVE
+
+function adjustLayout() {
+  const remInPixels = getRemInPixels();
+  const responsiveThreshold = InitialMainWidth + 2.5 * remInPixels; // InitialMainWidth + 1rem in px
+
+  if (window.innerWidth < responsiveThreshold) {
+    document.body.classList.add('responsive');
+  } else {
+    document.body.classList.remove('responsive');
+  }
+}
+
+window.addEventListener('resize', adjustLayout);
+
