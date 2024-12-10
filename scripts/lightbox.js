247834a5e5a220bfbacdbf64f42be4ba3463ea08
karlkuhne <karlkuhne@t-online.de>
2024-12-10 15:09:46 +0100

    errors

--- scripts/lightbox.js
+++ scripts/lightbox.js
@@ -1,16 +1,4 @@
-// LIGHTBOX OPEN
-  
-  document.querySelector('#project-opening-credits').onclick = () => {
-    document.querySelector('#lightbox-opening-credits').style.display = 'flex';
-    document.body.classList.add('no-scroll');
-  };
-  
-  document.querySelector('#project-term-paper').onclick = () => {
-    document.querySelector('#lightbox-term-paper').style.display = 'flex';
-    document.body.classList.add('no-scroll');
-  };
-  
-  
+
   // LIGHTBOX CLOSE
   
   const lightboxes = document.querySelectorAll('.lightbox');
