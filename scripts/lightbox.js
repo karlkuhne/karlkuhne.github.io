66e663f5de617c25a503201d98b1ac0dd2a456df
karlkuhne <karlkuhne@t-online.de>
2024-12-10 13:49:13 +0100

    removed yt player

--- scripts/lightbox.js
+++ scripts/lightbox.js
@@ -1,5 +1,4 @@
 // LIGHTBOX OPEN
-
   
   document.querySelector('#project-opening-credits').onclick = () => {
     document.querySelector('#lightbox-opening-credits').style.display = 'flex';
@@ -10,32 +9,6 @@
     document.querySelector('#lightbox-term-paper').style.display = 'flex';
     document.body.classList.add('no-scroll');
   };
-  
-  
-  
-  
-  // YOUTUBE PLAYER
-  
-  var player;
-  
-  function onYouTubeIframeAPIReady() {
-    player = new YT.Player('playerOpeningCredits', {
-      height: '100%',
-      width: '100%',
-      videoId: 'e4t05vSzYPg',
-      playerVars: {
-        'rel': 0, // Keine relativen Videos anzeigen
-        'controls': 1 // Zeige Video-Steuerelemente an
-      },
-      events: {
-        'onReady': onPlayerReady
-      }
-    });
-  }
-  
-  function onPlayerReady(event) {
-  
-  }
   
   
   // LIGHTBOX CLOSE
