59ccdf7162928cf4a36864d206fea073177009b8
karlkuhne <karlkuhne@t-online.de>
2024-12-14 21:34:25 +0100

    :)

---
+++ scripts/projectsData.js
@@ -0,0 +1,168 @@
+const projectsData = {
+    "projects": [
+        {
+            "label": "unity-spiel",
+            "category": "spiel",
+            "featured": true,
+            "thumbnail": "Ressources/Caveman_hits_Giraffe_in_a_Car/Thumbnail.avif",
+            "square": "Ressources/Caveman_hits_Giraffe_in_a_Car/Thumbnail_Square.avif",
+            "type": "slideshow",
+            "images": [
+                "Ressources/Caveman_hits_Giraffe_in_a_Car/1.avif",
+                "Ressources/Caveman_hits_Giraffe_in_a_Car/2.avif",
+                "Ressources/Caveman_hits_Giraffe_in_a_Car/3.avif",
+                "Ressources/Caveman_hits_Giraffe_in_a_Car/4.avif",
+                "Ressources/Caveman_hits_Giraffe_in_a_Car/5.avif"
+            ],
+            "apps": [
+                "Ressources/Icons/Unity.avif",
+                "Ressources/Icons/Rider.avif",
+                "Ressources/Icons/Procreate.avif",
+                "Ressources/Icons/GitHub.avif"
+            ],
+            "steam": "https://store.steampowered.com/widget/3381780/%22%20frameborder=%220%22%20width=%22646%22%20height=%22190",
+            "name": "Caveman hits Giraffe in a Car",
+            "description": "Ein rasantes Action-Roguelike, das aus einem 5-tägigen Game Jam entstanden ist. Kämpfe gegen abwechslungsreiche Gegnerwellen, verbessere deine Fähigkeiten und verewige dich auf der Bestenliste.",
+            "website": "https://cavemanhitsgiraffeinacar.github.io/",
+            "projectPage": "./projekte/cavemanhitsgiraffe"
+        },
+        {
+            "label": "portfolio-webseite",
+            "category": "spiel",
+            "featured": true,
+            "thumbnail": "Ressources/Portfolio_Website/Thumbnail.avif",
+            "square": "Ressources/Portfolio_Website/Thumbnail_Square.avif",
+            "type": "slideshow",
+            "images": [
+                "Ressources/Portfolio_Website/1.avif",
+                "Ressources/Portfolio_Website/2.avif",
+                "Ressources/Portfolio_Website/3.avif"
+            ],
+            "apps": [
+                "Ressources/Icons/VisualStudio.avif",
+                "Ressources/Icons/GitHub.avif",
+                "Ressources/Icons/XAMPP.avif"
+            ],
+            "name": "Portfolio Webseite",
+            "description": "Meine eigene Webseite, umgesetzt mit HTML, CSS und JavaScript mit einem textbasierten CMS zum Hochladen der Projekte. Die Seite wird als GitHub Page mit einer Custom Domain gehostet. Weiterhin habe ich diese bei Google indexiert und nach Pagespeed optimiert.",
+            "projectPage": "./projekte/portfolio-webseite"
+        },
+        {
+            "label": "character-design",
+            "category": "illustration",
+            "featured": false,
+            "thumbnail": "Ressources/Character_Design/Thumbnail.avif",
+            "square": "Ressources/Character_Design/Thumbnail_Square.avif",
+            "type": "slideshow",
+            "images": [
+                "Ressources/Character_Design/1.avif",
+                "Ressources/Character_Design/2.avif",
+                "Ressources/Character_Design/3.avif"
+            ],
+            "apps": [
+                "Ressources/Icons/Procreate.avif"
+            ],
+            "name": "Character Design",
+            "description": "Mein erstes Character Design „Hosbol Pogronop“, inspiriert von der Myconid-Rasse aus Dungeons and Dragons."
+        },
+        {
+            "label": "designing-for-immersion",
+            "category": "forschungsprojekt",
+            "featured": true,
+            "thumbnail": "Ressources/Term_Paper/Thumbnail.avif",
+            "square": "Ressources/Term_Paper/Thumbnail_Square.avif",
+            "type": "iframe",
+            "apps": [
+                "Ressources/Icons/Word.avif"
+            ],
+            "src": "projekte/designing-for-immersion.pdf",
+            "name": "Designing for immersion: The influence of diegetic player guidance on the gaming experience",
+            "description": "Gemäß des Grundsatzes „Designing for immersion“ ergründet diese Hausarbeit, ob Spielerführungsmechaniken in Videospielen besser Teil der erzählten Welt oder externes Spielelement sein sollten.",
+            "website": "projekte/designing-for-immersion.pdf"
+        },
+        {
+            "label": "vorspann",
+            "category": "film",
+            "featured": false,
+            "thumbnail": "Ressources/Opening_Credits/Thumbnail.avif",
+            "square": "Ressources/Opening_Credits/Thumbnail_Square.avif",
+            "type": "iframe",
+            "src": "https://www.youtube.com/embed/e4t05vSzYPg?rel=0",
+            "apps": [
+                "Ressources/Icons/Premiere.avif"
+            ],
+            "name": "Opening Credits",
+            "description": "Ein Vorspann für eine fiktive Serie, welcher die einzigartige Stimmung einer nächtlichen Stadt einfangen soll."
+
+        },
+        {
+            "label": "kubistische-portraits",
+            "category": "illustration",
+            "featured": false,
+            "thumbnail": "Ressources/Experimentelles_Projekt/Thumbnail.avif",
+            "square": "Ressources/Experimentelles_Projekt/Thumbnail_Square.avif",
+            "type": "slideshow",
+            "images": [
+                "Ressources/Experimentelles_Projekt/3.avif",
+                "Ressources/Experimentelles_Projekt/1.avif",
+                "Ressources/Experimentelles_Projekt/2.avif"
+            ],
+            "apps": [
+                "Ressources/Icons/Hand.avif"
+            ],
+            "name": "Cubism",
+            "description": "Eine Reihe von kubistischen Portraits, fiktive Personen, Pastellkreide auf Papier."
+        },
+        {
+            "label": "animationsfilm-kuh",
+            "category": "film",
+            "featured": false,
+            "thumbnail": "Ressources/Kuh/Thumbnail.avif",
+            "square": "Ressources/Kuh/Thumbnail_Square.avif",
+            "type": "iframe",
+            "src": "https://youtube.com/embed/J7LF3Q7Vtjg?rel=0",
+            "apps": [
+                "Ressources/Icons/Blender.avif",
+                "Ressources/Icons/Premiere.avif"
+            ],
+            "name": "Animationsfilm: Kuh",
+            "description": "In diesem Film wurde die Integration eines CGI-Elements in ein reales Video durchgeführt. Das Projekt umfasst die Erstellung und Animation eines 3D-Elements sowie das Tracking und die Integration dieses Objekts in ein selbst aufgenommenes Video.",
+            "projectPage": "./projekte/cow"
+        },
+        {
+            "label": "animationsfilm-object-terrors",
+            "category": "film",
+            "featured": false,
+            "thumbnail": "Ressources/Object_Terrors/Thumbnail.avif",
+            "square": "Ressources/Object_Terrors/Thumbnail_Square.avif",
+            "type": "iframe",
+            "src": "https://youtube.com/embed/aGy7EN_uFUs?rel=0",
+            "apps": [
+                "Ressources/Icons/Blender.avif",
+                "Ressources/Icons/Premiere.avif",
+                "Ressources/Icons/AfterEffects.avif"
+            ],
+            "name": "Animationsfilm: Object Terrors",
+            "description": "Das Konzept besteht darin, häufige menschliche Phobien auf alltägliche Haushalts- gegenstände zu übertragen. Um dieses Konzept audiovisuell umzusetzen, sollen fünf Gegenstände in derem jeweiligen Raum monologartig über deren Ängste sprechen. Der Sprechakt soll authentisch wirken, als wäre eine reale Person interviewt worden.",
+            "projectPage": "./projekte/objectTerrors"
+        },
+        {
+            "label": "isometrischer-raum",
+            "category": "illustration",
+            "featured": false,
+            "thumbnail": "Ressources/iso_room/Thumbnail.avif",
+            "square": "Ressources/iso_room/Thumbnail_Square.avif",
+            "type": "slideshow",
+            "images": [
+                "Ressources/iso_room/Render.avif"
+            ],
+            "apps": [
+                "Ressources/Icons/Blender.avif"
+            ],
+            "name": "Isometrischer Raum",
+            "description": "Mein erstes Projekt in Blender, rendered in Cycles."
+        }
+    ]
+}
+
+export default projectsData;
