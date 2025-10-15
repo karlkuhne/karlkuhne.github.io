5595ba0af6fe4e156400055f3b37ea11af904163
karlkuhne <karl.kuhne@digitalwert.de>
2025-10-16 00:05:50 +0200

    split in 2 svgs

--- components/Cover.vue
+++ components/Cover.vue
@@ -1,14 +1,26 @@
 <script setup lang="ts">
     import { ref, onMounted, onUnmounted } from 'vue';
 
-    const parallaxRef = ref<HTMLElement | null>(null);
+    const parallaxBackgroundRef = ref<HTMLElement | null>(null);
+    const parallaxLettersRef = ref<HTMLElement | null>(null);
 
     const handleScroll = () => {
         const scrollPosition = window.scrollY;
-        parallaxRef.value!.style.backgroundPositionY = `${scrollPosition * 0.15}px`;
+        
+        // Hintergrund (Minus-Pattern) bewegt sich langsamer
+        if (parallaxBackgroundRef.value) {
+            parallaxBackgroundRef.value.style.backgroundPositionY = `${scrollPosition * 0.1}px`;
+        }
+        
+        // Buchstaben bewegen sich schneller für mehr Tiefe
+        if (parallaxLettersRef.value) {
+            parallaxLettersRef.value.style.backgroundPositionY = `${scrollPosition * 0.15}px`;
+        }
 
-        const langSwitcher = parallaxRef.value!.querySelector('#languageSwitcher') as HTMLElement | null;
-        langSwitcher!.style.transform = `translateY(${scrollPosition * 0.15}px)`;
+        const langSwitcher = parallaxLettersRef.value?.querySelector('#languageSwitcher') as HTMLElement | null;
+        if (langSwitcher) {
+            langSwitcher.style.transform = `translateY(${scrollPosition * 0.15}px)`;
+        }
     };
 
     onMounted(() => {
@@ -21,26 +33,52 @@
 </script>
 
 <template>
-    <div id="parallax" ref="parallaxRef">
-        <LanguageSwitcher id="languageSwitcher" />
+    <div id="parallax-container">
+        <div id="parallax-background" ref="parallaxBackgroundRef"></div>
+        <div id="parallax-letters" ref="parallaxLettersRef">
+            <LanguageSwitcher id="languageSwitcher" />
+        </div>
     </div>
 </template>
 
 <style scoped lang="scss">
-    #parallax {
+    #parallax-container {
         position: relative;
         width: $screen-xl;
         margin: 0 auto;
         height: calc(100vh - 4rem);
-        background-image: url('~/assets/svg/CoverDesktop.svg');
-        background-size: cover;
-        background-position: center;
-        background-repeat: no-repeat;
         overflow: hidden;
     }
 
+    #parallax-background {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-image: url('~/assets/svg/CoverDesktopBackground.svg');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        will-change: background-position;
+    }
+
+    #parallax-letters {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-image: url('~/assets/svg/CoverDesktopLetters.svg');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        will-change: background-position;
+    }
+
     @include xl {
-        #parallax {
+        #parallax-background,
+        #parallax-letters {
             background-image: url('~/assets/svg/CoverMobile.svg');
         }
     }
