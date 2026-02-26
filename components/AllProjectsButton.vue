be9ce0a30598d064961cee339af171ac99080869
karlkuhne <karlkuhne@t-online.de>
2026-02-26 15:58:01 +0100

    Update AllProjectsButton.vue

--- components/AllProjectsButton.vue
+++ components/AllProjectsButton.vue
@@ -4,10 +4,11 @@
 
 <template>
     <div style="display: flex; justify-content: center;">
-        <NuxtLink :to="localePath('/projekt-galerie')" class="all-projects-button selectDisable">
+        <NuxtLink :to="localePath('/projekt-galerie')"
+            class="primary-btn flex items-center w-[8.7rem] hover:w-[9.7rem] transition-all duration-300 ease py-4 px-6 rounded-[1.5rem] cursor-pointer selectDisable group">
             <span>{{ $t('all_projects_button') }}</span>
-            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" height="17" width="17"
-                xmlns="http://www.w3.org/2000/svg">
+            <svg class="transition-transform duration-300 ease group-hover:translate-x-[1rem] ml-2.5"
+                stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" height="17" width="17" cxmlns="http://www.w3.org/2000/svg">
                 <path
                     d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z">
                 </path>
@@ -15,49 +16,3 @@
         </NuxtLink>
     </div>
 </template>
-
-<style scoped>
-    .all-projects-button {
-        display: inline-flex;
-        position: relative;
-        align-items: center;
-        overflow: hidden;
-
-        width: 8.7rem;
-        padding: 1rem 1.5rem 1rem 1.5rem;
-
-        background-color: rgb(11, 102, 194);
-        border: none;
-        border-radius: 1.5rem;
-
-        font-family: Work Sans;
-        font-size: 1rem;
-        color: white;
-        text-decoration: none;
-
-        transition: background-color 0.15s, width 0.3s ease;
-    }
-
-    .all-projects-button:hover {
-        width: 9.7rem;
-        background-color: rgb(5, 79, 155);
-        cursor: pointer;
-    }
-
-    .all-projects-button span {
-        transition: transform 0.3s ease;
-    }
-
-    .all-projects-button:hover span {
-        transform: translateX(-0.3rem);
-    }
-
-    .all-projects-button svg {
-        transition: transform 0.3s ease;
-        margin-left: 10px;
-    }
-
-    .all-projects-button:hover svg {
-        transform: translateX(1rem);
-    }
-</style>
