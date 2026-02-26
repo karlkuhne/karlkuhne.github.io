fa4d02fe4a5b75efb89e7a2e029da85f4f5bdc09
karlkuhne <karlkuhne@t-online.de>
2026-02-26 15:04:59 +0100

    Update LanguageSwitcher.vue

--- components/LanguageSwitcher.vue
+++ components/LanguageSwitcher.vue
@@ -32,10 +32,11 @@
 </script>
 
 <template>
-  <div class="relative w-fit h-fit flex bg-primary-70 rounded-md p-1">
-    <div class="absolute top-0 bottom-0 bg-primary-70 rounded-md" :style="highlightStyle" />
+  <div class="relative w-fit h-fit flex bg-primary-50 rounded-lg py-1">
+    <div class="absolute top-0 bottom-0 bg-primary-70 rounded-lg" :style="highlightStyle" />
 
-    <button v-for="(lang, index) in languages" :key="lang.code" class="flex gap-1.5" @click="changeLanguage(lang.code, index)"
-      ref="tabRefs"> <span class="icon">{{ lang.icon }}</span> {{ lang.label }} </button>
+    <button v-for="(lang, index) in languages" :key="lang.code" @click="changeLanguage(lang.code, index)"
+      class="text-base text-white bg-transparent border-none z-1 flex align-center justify-center gap-1.5 cursor-pointer"
+      ref="tabRefs"> <span class="text-base">{{ lang.icon }}</span> {{ lang.label }} </button>
   </div>
 </template>
