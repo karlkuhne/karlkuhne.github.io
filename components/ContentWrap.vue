852f13d40937bc02558ae3b27830a0dfaf045710
karlkuhne <karlkuhne@t-online.de>
2026-02-27 11:39:07 +0100

    Update ContentWrap.vue

--- components/ContentWrap.vue
+++ components/ContentWrap.vue
@@ -1,5 +1,7 @@
 <template>
-    <div id="content-wrap" class="w-full max-w-screen-md mx-auto md:text-justify line-height-7 mt-20 mb-28 [&_a]:text-accent-1 [&_a:hover]:text-accent-2">
+    <div id="content-wrap" class="w-full max-w-screen-md mx-auto md:text-justify line-height-7 mt-20 mb-28
+        [&_a]:text-accent-1 [&_a:hover]:text-accent-2
+        [&_img]:w-full [&_img]:rounded-lg">
         <slot />
     </div>
 </template>
